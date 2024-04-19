54679ae528cba0784d0a97ca84b7c1b6f86b1424
Christian Lehr <CLehr@timocom.com>
2024-04-19 18:00:52 +0200

    remove comments

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { signIn } from 'next-auth/react'; // Import signIn for logging in the user
+import { signIn } from 'next-auth/react'; 
 
-interface RegisterFormProps {
-  // Hier können Sie zusätzliche Props definieren, falls benötigt
-}
+interface RegisterFormProps {}
 
 const RegisterForm: React.FC<RegisterFormProps> = () => {
   const [email, setEmail] = useState<string>('');
