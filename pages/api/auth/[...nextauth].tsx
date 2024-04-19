3b9d399a70135d7779f71b55fc9222f31e3e6f35
Christian Lehr <CLehr@timocom.com>
2024-04-19 08:45:06 +0200

    change nextauth file

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth';
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { connectToDatabase } from '../../../lib/db';
 import { verifyPassword } from '../../../lib/auth';
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   secret: process.env.JWT_SECRET, // Use an environment variable for the JWT secret
   providers: [
     CredentialsProvider({
@@ -14,13 +15,13 @@ export default NextAuth({
       },
       async authorize(credentials) {
         const { db } = await connectToDatabase();
-        const user = await db.collection('users').findOne({ email: credentials.email });
+        const user = await db.collection('users').findOne({ email: credentials?.email });
 
         if (!user) {
           throw new Error('No user found!');
         }
 
-        const isValid = await verifyPassword(credentials.password, user.password);
+        const isValid = await verifyPassword(credentials?.password, user.password);
         if (!isValid) {
           throw new Error('Could not log you in!');
         }
@@ -28,19 +29,7 @@ export default NextAuth({
         return { email: user.email, id: user._id };
       }
     })
-  ],
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    session: async ({ session, token }) => {
-      if (token) {
-        session.user = { ...session.user, id: token.id };
-      }
-      return session;
-    },
-  },
-});
+  ]
+}
+
+export default NextAuth(authOptions);
