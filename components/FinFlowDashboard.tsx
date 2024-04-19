c72939f3016874d698dc09171fb134fb4735ccde
Christian Lehr <CLehr@timocom.com>
2024-04-19 17:32:29 +0200

    refactor

--- components/FinFlowDashboard.tsx
+++ components/FinFlowDashboard.tsx
@@ -3,7 +3,6 @@ import { useFinancialData } from '../contexts/FinancialDataContext';
 import { useUploads } from '../contexts/UploadsContext';
 import { DocumentModal } from './DocumentModal';
 
-
 interface FinancialData {
     date: string;
     earnings: number;
@@ -35,8 +34,8 @@ const FinFlowDashboard: React.FC = () => {
         const totalEarnings = data.reduce((acc, curr) => acc + curr.earnings, 0);
         const totalExpenses = data.reduce((acc, curr) => acc + curr.expenses, 0);
         const finalBalance = totalEarnings - totalExpenses;
-        const startDate = data.length > 0 ? formatDate(data[0].date) : '';
-        const endDate = data.length > 0 ? formatDate(data[data.length - 1].date) : '';
+        const startDate = data[0]?.date ? formatDate(data[0].date) : '';
+        const endDate = data[data.length - 1]?.date ? formatDate(data[data.length - 1].date) : '';
         setSummary({ totalEarnings, totalExpenses, finalBalance, startDate, endDate });
     };
 
@@ -72,33 +71,27 @@ const FinFlowDashboard: React.FC = () => {
         <div className="m-10 mx-auto px-4">
             <h1 className="text-3xl font-semibold text-center text-blue-600 mb-6">Dashboard</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {financialData.map((data, index) => (
-    <div key={index} className="bg-white shadow-md rounded-lg p-6">
-        <h2 className="text-xl font-bold mb-4">{formatDate(data.date)}</h2>
-        <p className="text-md mb-2">Einnahmen: <span className="font-semibold">{data.earnings.toFixed(2)}€</span></p>
-        <p className="text-md mb-2">Ausgaben: <span className="font-semibold">{data.expenses.toFixed(2)}€</span></p>
-        <p className="text-md mb-2">Saldo: <span className="font-semibold">{data.balance ? data.balance.toFixed(2) : '0.00'}€</span></p>
-        {
-            (() => {
-                const uploadsForDate = filterUploadsByDate(uploads, data.date);
-                if (uploadsForDate.length > 0) {
-                    return (
-                        <div>
-                            <h3 className="text-lg font-bold">Hochgeladene Rechnungen:</h3>
-                            <ul>
-                                {uploadsForDate.map((upload, uploadIndex) => (
-                                    <li key={uploadIndex} onClick={() => handleDocumentClick(upload)}>
-                                        <a href="#" onClick={(e) => e.preventDefault()}>{upload.originalName}</a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    );
-                }
-            })()
-        }
-    </div>
-))}
+                {financialData.map((data, index) => (
+                    <div key={index} className="bg-white shadow-md rounded-lg p-6">
+                        <h2 className="text-xl font-bold mb-4">{formatDate(data.date)}</h2>
+                        <p className="text-md mb-2">Einnahmen: <span className="font-semibold">{data.earnings.toFixed(2)}€</span></p>
+                        <p className="text-md mb-2">Ausgaben: <span className="font-semibold">{data.expenses.toFixed(2)}€</span></p>
+                        <p className="text-md mb-2">Saldo: <span className="font-semibold">{data.balance.toFixed(2)}€</span></p>
+                        {uploads.filter(upload => formatDate(upload.uploadDate) === formatDate(data.date)).length > 0 && (
+                            <div>
+                                <h3 className="text-lg font-bold">Hochgeladene Rechnungen:</h3>
+                                <ul>
+                                    {uploads.filter(upload => formatDate(upload.uploadDate) === formatDate(data.date))
+                                        .map((upload, uploadIndex) => (
+                                            <li key={uploadIndex} onClick={() => handleDocumentClick(upload)}>
+                                                <a href="#" onClick={(e) => e.preventDefault()}>{upload.originalName}</a>
+                                            </li>
+                                        ))}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                ))}
             </div>
             {selectedDocument && <DocumentModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} document={selectedDocument} />}
             {summary && (
@@ -107,7 +100,7 @@ const FinFlowDashboard: React.FC = () => {
                     <p className="text-md mb-2">Zeitraum: <span className="font-semibold">{summary.startDate} bis {summary.endDate}</span></p>
                     <p className="text-md mb-2">Gesamteinnahmen: <span className="font-semibold">{summary.totalEarnings.toFixed(2)}€</span></p>
                     <p className="text-md mb-2">Gesamtausgaben: <span className="font-semibold">{summary.totalExpenses.toFixed(2)}€</span></p>
-                    <p className="text-md">Aktuelles Saldo: <span className="font-semibold">{summary.finalBalance ? summary.finalBalance.toFixed(2) : '0.00'}€</span></p>
+                    <p className="text-md">Aktuelles Saldo: <span className="font-semibold">{summary.finalBalance.toFixed(2)}€</span></p>
                 </div>
             )}
         </div>
